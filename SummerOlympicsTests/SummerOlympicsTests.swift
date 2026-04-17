6db142d7d9d12d7caa2dfcc134fa33c9f53f803d
EGgery <eguzman0086@mymail.lausd.net>
2026-04-17 16:04:16 -0700

    Initial Commit

---
+++ SummerOlympicsTests/SummerOlympicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsTests.swift
+//  SummerOlympicsTests
+//
+//  Created by GUZMAN, ETHAN on 4/17/26.
+//
+
+import Testing
+@testable import SummerOlympics
+
+struct SummerOlympicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
